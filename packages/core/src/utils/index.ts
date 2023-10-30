26bbbd2b66de8539c5be61164cf0f4ec4506c422
Drew DeCarme <drewdecarme@gmail.com>
2023-10-30 14:23:41 -0400

    initial commit

---
+++ packages/core/src/utils/index.ts
@@ -0,0 +1,5 @@
+export * from "./util.error";
+export * from "./util.env";
+export * from "./util.types";
+export * from "./util.log";
+export * from "./util.validate";
---
+++ packages/docs/theme.config.jsx
@@ -0,0 +1,6 @@
+export default {
+  logo: <span>Flare City</span>,
+  project: {
+    link: 'https://github.com/drewdecarme/shank-city/tree/main/packages/flare-city/README.md'
+  }
+}
